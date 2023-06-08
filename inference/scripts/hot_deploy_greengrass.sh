a2f2fbf335e6f8f91213470f892cb83867aef453
Johannes Langer <jlanger@amazon.de>
2023-06-08 20:13:55 +0200

    Fixed scripts to hot deploy greengrass component

--- inference/scripts/hot_deploy_greengrass.sh
+++ inference/scripts/hot_deploy_greengrass.sh
@@ -2,18 +2,18 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ARTIFACT_BUCKET_NAME=$(aws cloudformation describe-stacks --query 'Stacks[?StackName==`MLOps-Init-Stack`] | [0].Outputs[?OutputKey==`mlopsDataBucket`][OutputValue][] ' --output=text)
-export COMPONENT_VERSION=$(python3 $SCRIPT_DIR/../inference/lib/assets/gg_component_version_helper/setup.py com.qualityinspection)
-export MODEL_VERSION=$(python3 $SCRIPT_DIR/../inference/lib/assets/gg_component_version_helper/setup.py com.qualityinspection.model | awk 'BEGIN{FS=OFS="."} {$3-=1} 1')
+export COMPONENT_VERSION=$(python3 $SCRIPT_DIR/../lib/assets/gg_component_version_helper/setup.py com.qualityinspection)
+export MODEL_VERSION=$(python3 $SCRIPT_DIR/../lib/assets/gg_component_version_helper/setup.py com.qualityinspection.model | awk 'BEGIN{FS=OFS="."} {$3-=1} 1')
 export TARGET_ARN=$(aws iot describe-thing --thing-name EdgeThing-MLOps-Inference-Statemachine-Pipeline-Stack  --output text --query 'thingArn')
 
-OUT_PATH=$SCRIPT_DIR/../inference/lib//cdk.out/greengrass_dev
+OUT_PATH=$SCRIPT_DIR/../cdk.out/greengrass_dev
 rm -rf ${OUT_PATH} && mkdir -p ${OUT_PATH}/artifacts ${OUT_PATH}/recipes
 
 # zip and upload artifact
-cd $SCRIPT_DIR/../inference/lib//assets/gg_components/artifacts/qualityinspection/; zip -r $SCRIPT_DIR/../inference/lib//cdk.out/greengrass_dev/artifacts/qualityinspection.zip . -x "*.DS_Store"
+cd $SCRIPT_DIR/../lib/assets/gg_components/artifacts/qualityinspection/; zip -r $SCRIPT_DIR/../cdk.out/greengrass_dev/artifacts/qualityinspection.zip . -x "*.DS_Store"
 aws s3 cp ${OUT_PATH}/artifacts/qualityinspection.zip s3://${ARTIFACT_BUCKET_NAME}/edge/artifacts/qualityinspection/${COMPONENT_VERSION}/
 # create recipe
-sed "s/BUCKET_NAME/${ARTIFACT_BUCKET_NAME}/g;s/COMPONENT_VERSION/${COMPONENT_VERSION}/g" $SCRIPT_DIR/../inference/lib//assets/gg_components/recipes/com.qualityinspection.json > ${OUT_PATH}/recipes/com.qualityinspection.json
+sed "s/BUCKET_NAME/${ARTIFACT_BUCKET_NAME}/g;s/COMPONENT_VERSION/${COMPONENT_VERSION}/g" $SCRIPT_DIR/../lib/assets/gg_components/recipes/com.qualityinspection.json > ${OUT_PATH}/recipes/com.qualityinspection.json
 # create deployment
 envsubst < $SCRIPT_DIR/gg-deployment.json > ${OUT_PATH}/gg-deployment.json
 
